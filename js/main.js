8f18eee74442766d6e85b1424edb1bca9a022ced
Ana Barros <barros.anacp@gmail.com>
2020-08-07 06:38:25 -0300

    Aprendendo functions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,65 @@
+
+function clicou(){
+    
+    //alert ("Obrigado por clicar");
+
+// ao invés de dar um alert, pode-se retornar um texto na tela referenciando uma tag por meio de uma id
+// <h3 id="agradecimento">Obrigado por clicar!</h3>     
+    
+    //document.getElementById("agradecimento"); 
+
+// ou ainda, usar funções para injetar o HTML, então a tag (seja <h> ou <p>) no HTML não deve ter um texto
+    document.getElementById("agradecimento").innerHTML = " <b>Obrigado por clicar!</b>"; //sendo um HTML pode-se formatar o texto como quiser
+    
+}
+
+function redirecionar(){
+    window.open("https://digitalinnovation.one/");
+    //ou
+    window.location.href = "https://digitalinnovation.one/"; //aqui não é função apenas altera a referência
+}
+
+/*
+// se usar variável global e local com o mesmo nome,
+// e pedir para o console imprimir, 
+//o resultado será referente à variável global
+
+var validar = 0; //global
+
+function validaIdade (idade) {
+    var validar; //local // se não declarar a variável local e deixar apenas a global, deve-se declarar a função após o prompt e antes do console (**)
+    if (idade >= 18) {
+        validar = true
+    } else {
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt ("Qual é a sua idade?");
+validaIdade (idade); //(**) 
+console.log (validar); //dará erro se a variável global só estiver declarada sem valor atribuído a ela, ou, se 
+
+console.log (validaIdade (idade)) //levará em conta a variável da função, pois está chamando a função
+*/
+
+/*
+//Alterar nome
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+*/
+
+/*
+//somar
+function soma (n1, n2) {
+    return n1 + n2;
+}
+alert(soma(5,10));
+*/
+
 /*
 //Ver data
 var d = new Date();
@@ -35,7 +97,7 @@ if (idade >= 18) {
 */
 
 /*
-//Usa-se prompt para imprimir pergunta sobre informações que seja necessárias,
+//Usa-se prompt para imprimir pergunta sobre informações que sejam necessárias,
 //por exemplo, sites que precisam saber idade do usuário
 var idade = prompt ("Qual sua idade?");
 if (idade >= 18) {
